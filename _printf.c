7c8c8e3baa45d1dab974a2c451a0c3bfc67f825d
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-21 18:52:22 +0200

    create anex file and main file

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _printf - function that reproduces the printf fctn
+ * @format: the string to be printed
+ * Return: int, the length of the string
+ */
+int _printf(const char *format, ...)
---
+++ anexfunctions.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of the string passed as parameter
+ * @str: the string passed to the fctn as param
+ * Return: int, number of element in the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
